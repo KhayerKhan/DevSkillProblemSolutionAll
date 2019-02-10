c98e859e1f6972510f97d4b5822fd594f8bcb83f
Khayer Khan <coderboy1010@gmail.com>
2019-02-10 14:47:11 -0800

    Adding problem solution no DCP_194

---
+++ DCP_194/DCP_194/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace DCP_194
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+           // string mobileNumber;
+                 
+            n = int.Parse(Console.ReadLine());
+
+            for(int i=0; i<n; i++)
+            {
+             string  mobileNumber = Console.ReadLine();
+
+                var Number = mobileNumber.Split(' ');
+                double mobile =Convert.ToDouble (Number[0]);
+
+                if (mobileNumber[0] == '8')
+                {
+                    if (mobileNumber[4] == '1')
+                    {
+                        Console.WriteLine($"{mobile} Citycell");
+                    }
+                    else if (mobileNumber[4] == '5')
+                    {
+                        Console.WriteLine($"{mobile} Teletalk");
+                    }
+                    else if (mobileNumber[4] == '6')
+                    {
+                        Console.WriteLine($"{mobile} Airtel");
+                    }
+                    else if (mobileNumber[4] == '7')
+                    {
+                        Console.WriteLine($"{mobile} Grameenphone");
+                    }
+                    else if (mobileNumber[4] == '8')
+                    {
+                        Console.WriteLine($"{mobile} Robi");
+                    }
+                    else if (mobileNumber[4] == '9')
+                    {
+                        Console.WriteLine($"{mobile} Banglalink");
+                    }
+                }
+                else if(mobileNumber[0]=='0')
+                {
+                    if (mobileNumber[2] == '1')
+                    {
+                        Console.WriteLine($"880{mobile} Citycell");
+                    }
+                    else if (mobileNumber[2] == '5')
+                    {
+                        Console.WriteLine($"880{mobile} Teletalk");
+                    }
+                    else if (mobileNumber[2] == '6')
+                    {
+                        Console.WriteLine($"880{mobile} Airtel");
+                    }
+                    else if (mobileNumber[2] == '7')
+                    {
+                        Console.WriteLine($"880{mobile} Grameenphone");
+                    }
+                    else if (mobileNumber[2] == '8')
+                    {
+                        Console.WriteLine($"880{mobile} Robi");
+                        //Console.WriteLine("v:880{0}", mobile );
+                    }
+                    else if (mobileNumber[2] == '9')
+                    {
+                        Console.WriteLine($"880{mobile} Banglalink");
+                    }
+
+
+                }
+                
+            }
+           
+
+        }
+    }
+}
